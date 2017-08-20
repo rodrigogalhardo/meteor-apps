f2ff311c3c228c7b770b26f75b10fe4e65bfa268
Rodrigo Galhardo <galhardo.dn@gmail.com>
2017-08-20 12:54:45 -0300

    update files and packages

--- recipes/collections/Recipes.js
+++ recipes/collections/Recipes.js
@@ -1,32 +1,31 @@
-import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
-
-Recipes = new Mongo.Collections('recipes');
-
+Recipes = new Meteor.Collection('recipes');
 RecipeSchema = new SimpleSchema({
-    name:{
-        type:String,
-        label:"Name"
+    name: {
+        type: String,
+        label: "Name"
     },
-    desc:{
-        type:String,
-        label:"Description"
+    desc: {
+        type: String,
+        label: "Description"
     },
-    author:{
-        type:String,
-        label:"Author",
-        autoValue:function(){
-            return this.userId;
+    author: {
+        type: String,
+        label: "Author",
+        autoValue: function () {
+            return this.userId
         },
         autoForm:{
             type:"hidden"
         }
     },
-    createdAt:{
-        type:Date,
-        label:"CreatedAt",
-        autoValue:function(){
-            return new Date();
+    createdAt: {
+        type: Date,
+        label: "CreatedAt",
+        autoValue: function () {
+            return new Date()
+        },
+        autoForm:{
+            type:"hidden"
         }
     }
 });
